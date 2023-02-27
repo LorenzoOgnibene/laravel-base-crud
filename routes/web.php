ce8d02d0c9ddb17f958efecd87c7ba811a6a8c0f
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-02-27 11:20:51 +0100

    added controller and create route on admin page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\BookController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +22,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function (){
+    Route::resource('/books', BookController::class);
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
