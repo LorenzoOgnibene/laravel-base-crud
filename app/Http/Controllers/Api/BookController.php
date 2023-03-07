707458bc96d4ab4794cdef45a5c33d7f8a3e6420
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-07 15:54:08 +0100

    Create method 'index' into class controller 'BookController'

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index() {
+        $books = Book::all();
+        return response()->json([
+            'success' => true,
+            'results' => $books
+        ]);
+    }
 }
