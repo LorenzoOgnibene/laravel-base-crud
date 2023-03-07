087bbc32e940429a1253cedc7e55ef8218be1a8b
Paolo224 <picariellopaolo224@gmail.com>
2023-03-07 17:57:51 +0100

    add TypeSeeder in DataBaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            TypeSeeder::class,
             BooksTableSeeder::class
         ]);
     }
