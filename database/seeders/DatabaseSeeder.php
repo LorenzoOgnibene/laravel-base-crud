bc76f948d3b9d5a71e70caba29fd747bbe518339
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-07 17:43:45 +0100

    Edit method 'run' into class seeder ' DatabaseSeeder'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             BooksTableSeeder::class,
-            ResellersSeeder::class
+            ResellersSeeder::class,
+            BooksResellersSeeder::class
         ]);
     }
 }
